7226a984c53452bc3e1e238f9d7fc4ff5214a601
ecardona506 <ecardona506@javerianacali.edu.co>
2019-07-28 14:06:00 -0500

    Update The ?1?2?...?n = k Problem.py

--- The ?1?2?...?n = k Problem.py
+++ The ?1?2?...?n = k Problem.py
@@ -1,41 +1,36 @@
-
 from sys import stdin
-import math,sys
 
-def sequence_lenght(n,ans):
-	global acum
-	if n <= 9:
-		return ans + (int(n)*(int(n)+1))>>1
-	exp = int(math.log(n,10))
-	x = int(n-(10**exp) + 1)
-	ans += int(exp*((x*(x+1))>>1))
-	return sequence_lenght(x,ans)
-
-def binary_search(low,hi,i):
+def binary_search(n,low,hi):
 	mid = (low+hi)>>1
-	if low + 1 == hi:
-		length = sequence_lenght(low,0)
-		exp = int(math.log(low,10))
-		string = ""
-		for i in range(1,low+1):
-			string += str(i)
-		ans = string[length-i-2]
-		print(ans)
-	else:
-		length = sequence_lenght(mid,0)
-		if i > length: binary_search(mid,hi,i)
-		elif i <= length: binary_search(low,mid,i)
+	if low+1 == hi:
+		value = (low*(low+1))>>1
+		if value == n:
+			ans = low
+		else:
+			low+=1
+			value = (low*(low+1))>>1
+			#Si se resta algun valor, el nuevo valor es value - 2 veces el valor a restar
+			while (value - n)%2:
+				low+=1
+				value = (low*(low+1))>>1
+			ans = low
+		return ans 
+	value = (mid*(mid+1))>>1
+	if value > n: return binary_search(n,low,mid)
+	else: return binary_search(n,mid,hi)
 
 def main():
-	global string
 	cases = int(stdin.readline().strip())
-	string = ""
-	for i in range(1,8977):
-		string += str(i)
-	"""
-	while cases:
-		i = int(stdin.readline().strip())
-		binary_search(1,8977,i)
-		cases-=1
-	"""
+	while cases != 1:
+		stdin.readline()
+		n = int(stdin.readline().strip())
+		ans = binary_search(abs(n),1,44721)
+		print(ans)
+		print()
+		cases -=1
+	stdin.readline()
+	n = int(stdin.readline().strip())
+	ans = binary_search(abs(n),1,44721)
+	print(ans)
+
 main()
